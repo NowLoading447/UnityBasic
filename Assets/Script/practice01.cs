e6550ed607bbbda48f2e2cd2b588a1f4f53aaea6
NowLoading447 <finson@naver.com>
2025-06-05 21:19:40 +0900

    makescript
    makescript

---
+++ Assets/Script/practice01.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class practice01 : MonoBehaviour
+{
+    public string myName = "조민식";
+    public int age = 30;
+    public float height = 185.0f;
+    public bool student = true;
+    public string mbti = "ENTP";
+    public string gender = "남성";
+    public string[] hobbies = { "게임", "독서", "게임 기획"};
+    public string favColor = "청록색";
+    public int birthday = 19960611;
+    public string saysomthing = "조민식이라고 합니다. 잘 부탁드립니다.";
+
+    void print()
+    {
+        Debug.Log("이름: " + myName);
+        Debug.Log("나이: " + age);
+        Debug.Log("키: " + height + "cm");
+        Debug.Log("경일 게임 아카데미 학생인가?: " + student);
+        Debug.Log("MBTI: " + mbti);
+        Debug.Log("성별: " + gender);
+        Debug.Log("취미: " + string.Join(", ", hobbies));
+        Debug.Log("좋아하는 색: " + favColor);
+        Debug.Log("생일: " + birthday);
+        Debug.Log("한마디: " + saysomthing);
+    }
+
+    void Start()
+    {
+        print();
+    }
+}
